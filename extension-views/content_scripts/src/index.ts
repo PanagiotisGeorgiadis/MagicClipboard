a8f0c1afeb3e73220d6dbe9eb7e92b0cc17837a3
Panagiotis Georgiadis <panagiwtis.geor@gmail.com>
2020-11-19 12:43:21 +0000

    Improving the content_script by searching for the document active element inside iframes and shadow roots

--- extension-views/content_scripts/src/index.ts
+++ extension-views/content_scripts/src/index.ts
@@ -1,10 +1,34 @@
+/**
+ * Return the active element of a page, regardless of shadow root or iframe window.
+ * @returns {HTMLElement}
+ */
+function getActiveElement(element: any = document.activeElement) {
+  if (element) {
+    const shadowRoot = element.shadowRoot
+    const contentDocument = element.contentDocument
+
+    if (shadowRoot && shadowRoot.activeElement) {
+      return getActiveElement(shadowRoot.activeElement)
+    }
+
+    if (contentDocument && contentDocument.activeElement) {
+      return getActiveElement(contentDocument.activeElement)
+    }
+
+    return element
+  }
+}
+
 chrome.runtime.onMessage.addListener(request => {
   // InsertValue is used to paste a value to the active element.
   if (request.kind === "InsertValue") {
-    const element = document.activeElement as HTMLInputElement
-
-    if (element && element.value !== undefined) {
-      element.value = request.value
+    const activeElement = getActiveElement()
+    if (activeElement && activeElement.value !== undefined) {
+      activeElement.value = request.value
+    } else {
+      console.error(
+        "Magic Clipboard failed to detect the active element on the current window."
+      )
     }
   }
 })
